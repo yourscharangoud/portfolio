ea63335dbe5a3de5f72ca4da05343da787dfae2f
Charan <yourscharangoud@gmail.com>
2025-01-16 11:51:23 +0530

    first commit

---
+++ src/app/contact/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <div className="flex flex-col justify-center items-center text-center h-screen">
+        <h1 className="text-4xl">Contact Page</h1>
+      </div>
+    );
+  }
+    
